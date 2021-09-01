cfa6e0ae5c0a8ba1cfcd3abb8ceb176a9c533e2b
Neill Pearman <n.pearman@gmail.com>
2021-09-01 09:39:20 +0200

    Update exercise setup

--- exercises/plainJavascript.js
+++ exercises/plainJavascript.js
@@ -1,14 +1,13 @@
-const message = 'Hello, JavaScript!';
+const message = 'Hello, JavaScript!'
 
-let messages = [];
+let messages = []
 
 for (let i = 0; i < 3; i++) {
-  messages.push(`${message} ${i + 1}`);
+  messages.push(`${message} ${i + 1}`)
 }
 
-function formatMessages(messages) {
-  console.log(messages.join('\n'));
+function formatMessages (messages) {
+  console.log(messages.join('\n'))
 }
 
-// Note that there is deliberately a typo in this function call
-formatMessage(messages);
+formatMessage(messages)
